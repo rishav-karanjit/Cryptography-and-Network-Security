0f4f12d2278ef602f1a0a2917dca52e13c3456a1
unknown <karanjitrishav4@gmail.com>
2020-10-06 16:38:46 +0545

    step 1 complete Hill cipher

--- 3. Hill Cipher/HillCipher.cpp
+++ 3. Hill Cipher/HillCipher.cpp
@@ -4,9 +4,29 @@
 using namespace std;
 
 int a[50];
+int a2x2[25][25];
+
+//Changed plain txt to number
 void Txt_to_num(string str){
 	for (int i = 0; i < str.size(); i++){
     	a[i] = str[i]-97;
+	}
+}
+
+//Converted matrix a to ? * 2 form
+void convert_two_mat(int s){
+	int r,c;
+	for(int i=0;i<s;i++){
+		if((i+1)%2)
+			r++;
+		r=0;
+		c=0;
+		a2x2[r][c++]=a[i];
+		// cout<<a2x2[r][c-1];
+	}
+	if(c==1){
+		a2x2[r][c]=999;
+		// cout<<a2x2[r][c];
 	}
 }
 
@@ -18,7 +38,8 @@ int main(){
 	
 	Txt_to_num(p);
 
-	for(i=0;i<p.size();i++){
-		cout<<a[i];
-	}
+	// for(i=0;i<p.size();i++){
+	// 	cout<<a[i];
+	// }
+	convert_two_mat(p.size());
 }
